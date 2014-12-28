be0a0a80feabeac5e8969200826d6389d7831e37
yuki3738 <y.minamiya@gmail.com>
2014-12-29 01:49:39 +0900

    次はP15 3.モデルスペック

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you've limited to :test, :developmena,sor :production.
 Bundler.require(*Rails.groups)
 
 module ContactsRspec3Rails41
@@ -19,5 +19,15 @@ module ContactsRspec3Rails41
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
   end
 end
