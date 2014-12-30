11e633620855317323faba0ffe544be74cc92056
yuki3738 <y.minamiya@gmail.com>
2014-12-31 04:03:09 +0900

    次はP52 newから

---
+++ spec/controllers/contacts_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe ContactsController do
+
+  describe 'GET #index' do
+    context 'with params[:letter]' do
+      it "populates an array of contacts starting with the letter" do
+        smith = create(:contact, lastname: 'Smith')
+        jones = create(:contact, lastname: 'Jones')
+        get :index, letter: 'S'
+        expect(assigns(:contacts)).to match_array([smith])
+      end
+
+      it "renders the :index template" do
+        get :index, letter: 'S'
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'without params[:letter]' do
+      it "populates an array of all contacts" do
+        smith = create(:contact, lastname: 'Smith')
+        jones = create(:contact, lastname: 'Jones')
+        get :index
+        expect(assigns(:contacts)).to match_array([smith, jones])
+      end
+    end
+
+      it "renders the :index template" do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
+  describe 'GET #show' do
+    it "assigns the requested contact to @contact" do
+      contact = create(:contact)
+      get :show, id: contact
+      expect(assigns(:contact)).to eq contact
+    end
+
+    it "renders the :show template" do
+      contact = create(:contact)
+      get :show, id: contact
+      expect(response).to render_template :show
+    end
+  end
+end
