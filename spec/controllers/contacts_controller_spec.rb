19e5438ac6bee19d4d047e4b37ca7094833bef1e
yuki3738 <y.minamiya@gmail.com>
2014-12-31 17:21:18 +0900

    次はP55 PATCH リクエストをテストする

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -71,4 +71,29 @@ describe ContactsController do
       expect(response).to render_template :edit
     end
   end
+
+  describe 'GET #edit' do
+    before :each do
+      @phones = [
+        attributes_for(:phone),
+        attributes_for(:phone),
+        attributes_for(:phone)
+      ]
+    end
+
+    context "with valid attributes" do
+      it "saves the new contact in the database" do
+        expect{
+          post :create, contact: attributes_for(:contact,
+            phones_attributes: @phones)
+        }.to change(Contact, :count).by(1)
+      end
+
+      it "redirects to contacts#show" do
+        post :create, contact: attributes_for(:contact,
+          phones_attributes: @phones)
+        expect(response).to redirect_to contact_path(assigns[:contact])
+      end
+    end
+  end
 end
